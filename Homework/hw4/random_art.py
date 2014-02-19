ce78fd0788fae8b1d6340ea17de7ff42a8c3a754
Sophia Li <sophia.li@students.olin.edu>
2014-02-19 15:44:31 -0500

    I made beautiful art so happy so happy

--- Homework/hw4/random_art.py
+++ Homework/hw4/random_art.py
@@ -2,9 +2,8 @@
 """
 Created on Tue Feb 11 11:34:57 2014
 
-@author: sophie li wat is recursion how do i do it
-omg why
-why
+@author: sophie li i am artist
+collaboration with Zoher and Jay Woo
 """
 
 # you do not have to use these particular modules, but they may help
@@ -12,36 +11,17 @@ from random import randint
 import Image
 from math import *
 
-#
-#def snow_flake_side(turtle, l, level):
-#    """ Draw a side of the snowflake curve with side length l and recursion depth of level """
-#    angle_c = 60
-#    if level == 0:
-#        print "Forward"
-#        print l
-#        fd(bob, dist = l)
-#        return
-#    #fd(bob, dist=l) 
-#    snow_flake_side(turtle,(1.0/3)*l,level-1)
-#    lt(bob, angle=-1 * angle_c)
-#    snow_flake_side(turtle,(1.0/3)*l,level-1)
-#    lt(bob, angle=2*angle_c)
-#    snow_flake_side(turtle,(1.0/3)*l,level-1)
-#    lt(bob, angle=-1 * angle_c)
-#    snow_flake_side(turtle,(1.0/3)*l,level-1)
-#    lt(bob, angle=-1 * 360/level)
-
-#snow_flake_side(bob, 300,2)
-
 def build_random_function(min_depth, max_depth):
-        # your doc string goes here
+    """Takes minimum function depth and maximum function depth as inputs, and 
+    generates a function using a set of 6 "blocks".
+    """
     hello = ['x','y']
-    func = ['x','y','cos_pi','sin_pi','prod']
+    func = ['x','y','cos_pi','sin_pi','prod','square','average']
     if max_depth == 1:
         return hello[randint(0,1)]
     else:
-        block = func[randint(2,4)]
-        if block == 'prod':
+        block = func[randint(2,6)]
+        if block == 'prod' or 'average':
             return [block, build_random_function(min_depth-1, max_depth-1), build_random_function(min_depth-1, max_depth-1)]
         elif not block == 'prod':
            return [block, build_random_function(min_depth-1, max_depth-1)] 
@@ -49,40 +29,81 @@ def build_random_function(min_depth, max_depth):
 #print build_random_function(2,5)
 
 def evaluate_random_function(f, x, y):
-    
-    func1 = f[0]
-    
-    if func1 == 'x':
+    """Takes function as input and a numerical value for x and y. 
+    This function evalues the input function using the values provided for x and y.
+    """    
+    if f[0] == 'x':
         return x
-    elif func1 == 'y':
+    elif f[0] == 'y':
         return y
-        
-    func2 = f[1]
+    elif f[0] == 'square':
+        return evaluate_random_function(f[1],x,y)**2
+    elif f[0] == 'average':
+        return (evaluate_random_function(f[1],x,y)+evaluate_random_function(f[2],x,y))/2
+    elif f[0] == 'cos_pi':
+        return cos(pi*evaluate_random_function(f[1],x,y))
+    elif f[0] == 'sin_pi':
+        return sin(pi*evaluate_random_function(f[1],x,y))
+    elif f[0] == 'prod':
+        return evaluate_random_function(f[1],x,y)*evaluate_random_function(f[2],x,y)
 
+
+#print evaluate_random_function(['sin_pi', ['cos_pi', ['cos_pi', ['sin_pi', ['y']]]]],0.1,0.9)
+  
+def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
+    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
+        to the output interval [output_interval_start, output_interval_end].  The mapping
+        is an affine one (i.e. output = input*c + b).
+    
+        TODO: please fill out the rest of this docstring
+    """
+    output_interval = output_interval_end - output_interval_start
+    output_interval = float(output_interval)
+    input_interval = input_interval_end-input_interval_start
+    scaled_val = (output_interval*(val - input_interval_start)/(input_interval)) + output_interval_start
+    return scaled_val
+
+#print remap_interval(75,50,100,0,1)
+
+def create_image():
+    """This function takes no inputs since we are generating a random image 
+    based on previously defined functions. It will generate separate functions 
+    and pixel values for each color channel and combine them in the final image.
+    """
+    red_func =  build_random_function(2,4)
+    blue_func = build_random_function(2,4)
+    green_func = build_random_function(2,4)
+    
+    img = Image.new("RGB", (1000,1000))
+    pixels = img.load()
+    
+    for x in range(350):
+        x_map = remap_interval(x,1,349,-1,1) #since there are 350X350 p
+        for y in range(350):
+            y_map = remap_interval(y,1,349,-1,1)
+            red = evaluate_random_function(red_func,x_map,y_map)
+            red_map = remap_interval(red,-1,1,0,255)
+            green = evaluate_random_function(green_func,x_map,y_map)
+            green_map = remap_interval(green,-1,1,0,255)
+            blue = evaluate_random_function(blue_func,x_map,y_map)
+            blue_map = remap_interval(blue,-1,1,0,255)
+            pixels[x,y] = (int(red_map),int(green_map),int(blue_map))
+    img.show()
+
+create_image()
 
     
-    if func1 == 'prod':
-        func3 = f[2]
-     
-        
-    elif func1 == 'cos_pi':
-        return cos(pi*evaluate_random_function(func2,x,y))
-    elif func1 == 'sin_pi':
-        return sin(pi*evaluate_random_function(func2,x,y))
-    elif func1 == 'prod':
-        return evaluate_random_function(func2,x,y)*evaluate_random_function(func3,x,y)
-
     
-
-    # your code goes here
-
-print evaluate_random_function(['sin_pi', ['cos_pi', ['cos_pi', ['sin_pi', ['y']]]]],0.1,0.9)
-#def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
-#    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
-#        to the output interval [output_interval_start, output_interval_end].  The mapping
-#        is an affine one (i.e. output = input*c + b).
-#    
-#        TODO: please fill out the rest of this docstring
-#    """
-#    # your code goes here
-#    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
